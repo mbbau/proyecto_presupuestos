ca1fe847cd9497bfacc45cdf2bb54199eab8fbbe
Matias Baudino <mbbaudino@gmail.com>
2024-04-17 13:01:37 -0300

    Cambié el layout de la página por wide.

--- quotations_app.py
+++ quotations_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(
+    page_title= "Quotations_project",
+    layout= "wide"
+)
+
 columna_logo = st.columns(3)
 with columna_logo[2]:
     st.image('logo clinica.png')
